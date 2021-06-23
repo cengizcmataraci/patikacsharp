d00b6852bf1a6e8593ba6aa4a92289070b5926f9
cengizcmataraci <cengizcmataraci@gmail.com>
2021-06-23 21:18:00 +0300

    initial commit

---
+++ console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi girin: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı girin: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine($"Selam, {name} {surname}"); //string interpolation
+            Console.ReadKey();
+        }
+    }
+}
