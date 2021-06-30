b9f348701fc35ad5547ab8e1a83721dd97584181
cengizcmataraci <cengizcmataraci@gmail.com>
2021-06-30 19:24:09 +0300

    add variables section

---
+++ operators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Atama ve İşlemli Atama
+            int x = 5;
+            int y = 3;
+            y = y + 2;
+            Console.WriteLine(y);
+            y += 2; // İşlemli atama
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            // Mantıksal Operatörler
+            // ||, &&, !
+            bool isSuccess = true;
+            bool isCompleted = false;
+        }
+    }
+}
