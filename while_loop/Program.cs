4d044e0db9a4881db0ef96cb69318b37af483385
cengizcmataraci <cengizcmataraci@gmail.com>
2021-07-03 00:17:10 +0300

    add while_loop

---
+++ while_loop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace while_loop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        // While
+        // 1'den başlayarak girilen sayıya kadar ortalama hesaplamak
+        Console.WriteLine("Sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+        while (sayac <= sayi)
+        {
+            toplam += sayac;
+            sayac++;
+        }
+        Console.WriteLine(toplam / sayi);
+
+        // a'dan z'ye kadar tüm harfleri yazdır
+        char character = 'a';
+        while (character <= 'z')
+        {
+            Console.WriteLine(character);
+            character++;
+        }
+
+        // Foreach
+        string[] arabalar = {"Clio", "Hyundai", "Ferrari", "Mustang", "BMW"};
+
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+  }
+}
