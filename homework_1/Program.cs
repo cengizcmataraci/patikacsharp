1aba959b9b516f60ae6f295c3c4e4e7828b469cd
cengizcmataraci <cengizcmataraci@gmail.com>
2021-07-21 21:50:04 +0300

    add other sections

--- homework_1/Program.cs
+++ homework_1/Program.cs
@@ -9,20 +9,21 @@ namespace homework_1
       Console.WriteLine("*** 1 ***");
       Console.WriteLine("Lütfen pozitif bir sayı giriniz: ");
       int sayi = int.Parse(Console.ReadLine());
+      int[] sayiDizisi = new int[sayi];
+
       if (sayi > 0)
       {
-        int[] sayiDizisi = new int[sayi];
         Console.WriteLine($"Lütfen {sayi} tane sayı giriniz: ");
         for (int i = 0; i < sayiDizisi.Length; i++)
         {
           Console.Write($"{i + 1}. sayı: ");
           sayiDizisi[i] = int.Parse(Console.ReadLine());
         }
-        foreach (var sayi1 in sayiDizisi)
+        foreach (var sayilar in sayiDizisi)
         {
-          if (sayi1 % 2 == 0)
+          if (sayilar % 2 == 0)
           {
-            Console.WriteLine(sayi1);
+            Console.WriteLine(sayilar);
           }
         }
       }
@@ -30,6 +31,65 @@ namespace homework_1
       {
         Console.WriteLine("İstenilen şekilde sayı girmediniz!");
       }
+
+      Console.WriteLine("*** 2 ***");
+      Console.WriteLine("Lütfen pozitif iki sayı giriniz.");
+
+      Console.Write("Birinci sayı: ");
+      int sayi1 = int.Parse(Console.ReadLine());
+
+      Console.Write("İkinci sayı: ");
+      int sayi2 = int.Parse(Console.ReadLine());
+
+      Console.WriteLine($"Lütfen {sayi1} tane pozitif sayı giriniz.");
+      int[] kiyaslamaDizisi = new int[sayi1];
+
+      for (int i = 0; i < kiyaslamaDizisi.Length; i++)
+      {
+        Console.Write($"{i + 1}. sayı: ");
+        kiyaslamaDizisi[i] = int.Parse(Console.ReadLine());
+      }
+      foreach (var bolunenSayi in kiyaslamaDizisi)
+      {
+        if (bolunenSayi == sayi2 || bolunenSayi % sayi2 == 0)
+        {
+          Console.WriteLine(bolunenSayi);
+        }
+      }
+
+      Console.WriteLine("*** 3 ***");
+      Console.Write("Lütfen pozitif bir sayı giriniz: ");
+
+      int kelimeSayisi = int.Parse(Console.ReadLine());
+      Console.WriteLine($"Lütfen {kelimeSayisi} adet kelime giriniz.");
+
+      string[] kelimeDizisi = new string[kelimeSayisi];
+      for (int i = 0; i < kelimeDizisi.Length; i++)
+      {
+        Console.Write($"{i + 1}. kelime: ");
+        kelimeDizisi[i] = Console.ReadLine();
+      }
+
+      Array.Reverse(kelimeDizisi);
+
+      foreach (var kelime in kelimeDizisi)
+      {
+        Console.WriteLine(kelime);
+      }
+
+      Console.WriteLine("*** 4 ***");
+      Console.Write("Lütfen bir cümle yazınız: ");
+
+      string cumle = Console.ReadLine();
+      string[] parcalanmisCumle = cumle.Split(' ');
+      int toplam = 0;
+
+      foreach (string cumleler in parcalanmisCumle)
+      {
+        toplam += cumleler.Length;
+      }
+      Console.WriteLine("Toplam harf sayısı: " + toplam);
+      Console.WriteLine("Toplam kelime sayısı: " + cumle.Split().Length);
     }
   }
 }
