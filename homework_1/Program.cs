4a5d80a6091cd91f1bdaa0013240704a10f4d4d9
cengizcmataraci <cengizcmataraci@gmail.com>
2021-07-21 20:32:55 +0300

    add homework_1 && first section

---
+++ homework_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace homework_1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("*** 1 ***");
+      Console.WriteLine("Lütfen pozitif bir sayı giriniz: ");
+      int sayi = int.Parse(Console.ReadLine());
+      if (sayi > 0)
+      {
+        int[] sayiDizisi = new int[sayi];
+        Console.WriteLine($"Lütfen {sayi} tane sayı giriniz: ");
+        for (int i = 0; i < sayiDizisi.Length; i++)
+        {
+          Console.Write($"{i + 1}. sayı: ");
+          sayiDizisi[i] = int.Parse(Console.ReadLine());
+        }
+        foreach (var sayi1 in sayiDizisi)
+        {
+          if (sayi1 % 2 == 0)
+          {
+            Console.WriteLine(sayi1);
+          }
+        }
+      }
+      else
+      {
+        Console.WriteLine("İstenilen şekilde sayı girmediniz!");
+      }
+    }
+  }
+}
