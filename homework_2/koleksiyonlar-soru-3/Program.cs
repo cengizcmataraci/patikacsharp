fcd62ca9bf9678100abab257b224def0f10ffdfd
cengizcmataraci <cengizcmataraci@gmail.com>
2021-07-22 23:34:13 +0300

    add homework_2 section 3

---
+++ homework_2/koleksiyonlar-soru-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace koleksiyonlar_soru_3
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<char> vowels = new List<char>();
+      Console.WriteLine("Lütfen bir cümle giriniz: ");
+      string sentence = Console.ReadLine().ToLower();
+      char[] splitted = sentence.ToCharArray();
+      for (int i = 0; i < splitted.Length; i++)
+      {
+        if (splitted[i] == 'a' || splitted[i] == 'e' || splitted[i] == 'ı' || splitted[i] == 'i' || splitted[i] == 'o' || splitted[i] == 'ö' || splitted[i] == 'u' || splitted[i] == 'ü')
+        {
+          vowels.Add(splitted[i]);
+        }
+      }
+
+      vowels.Sort();
+
+      foreach (var item in vowels)
+      {
+        Console.WriteLine(item);
+      }
+
+    }
+  }
+}
