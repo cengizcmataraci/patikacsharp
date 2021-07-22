0f139bf21e8f90c2333fdbaf5eed35694322049e
cengizcmataraci <cengizcmataraci@gmail.com>
2021-07-22 21:05:44 +0300

    add homework_2 section 2

---
+++ homework_2/koleksiyonlar-soru-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace koleksiyonlar_soru_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] numberList = new int[20];
+      Console.WriteLine("Lütfen 20 sayı giriniz: ");
+
+      for (int i = 0; i < 20; i++)
+      {
+        Console.Write($"{i + 1}. sayı: ");
+        numberList[i] = int.Parse(Console.ReadLine());
+      }
+
+      Array.Sort(numberList);
+
+      int averageMin = (numberList[0] + numberList[1] + numberList[2]) / 3;
+      int averageMax = (numberList[17] + numberList[18] + numberList[19]) / 3;
+      int averageSum = averageMax + averageMin;
+
+      Console.WriteLine("En küçük üç sayının ortalaması: " + averageMin);
+      Console.WriteLine("En büyük üç sayının ortalaması: " + averageMax);
+      Console.WriteLine("Ortalamalar toplamı: " + averageSum);
+    }
+  }
+}
